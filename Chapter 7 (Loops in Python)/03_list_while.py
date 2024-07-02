3261aed1f2bed07d235d183aee252785510c3bf6
Charchit <charchitregmi10@gmail.com>
2024-07-02 14:33:34 +0545

    Minor Changes

--- Chapter 7 (Loops in Python)/03_list_while.py
+++ Chapter 7 (Loops in Python)/03_list_while.py
@@ -1,4 +1,4 @@
-#Printing the contents of a list using 'while' loop
+# Printing the contents of a list using 'while' loop
 
 p = ['Julian', 'Cole', 'Riyad', 'Mcatee']
 v = len(p)
@@ -6,3 +6,10 @@ i=0
 while (i<v):
     print(p[i])
     i=i+1 
+
+# Printing even numbers from 1 to 20 using 'while' loop
+a = 1
+while (a<21):
+    if a%2==0:
+        print(a)
+    a+=1    
